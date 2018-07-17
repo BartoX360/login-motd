1b154011bc3251e13a277641f1d7b617d7ab5f42
Bartox <me@bartox.fr>
2018-07-17 11:41:27 +0200

    Passage en français + ajout du check de mises à jour disponible

--- motd.sh
+++ motd.sh
@@ -2,6 +2,7 @@
 # Description: MOTD for Debian/Ubuntu
 # 2013/07/15 @ysaotome
 # 2014/05/29 @frdmn
+# 2018/07/17 @Bartox Passage en français + ajout du check de mises à jour disponible
 
 BIN_DATE='/bin/date'
 BIN_FIGLET='/usr/bin/figlet'
@@ -11,6 +12,7 @@ BIN_GREP='/bin/grep'
 BIN_SED='/bin/sed'
 BIN_UPTIME='/usr/bin/uptime'
 BIN_CAT='/bin/cat'
+BIN_LSB_RELEASE='/usr/bin/lsb_release'
 BIN_UNAME='/bin/uname'
 BIN_FREE='/usr/bin/free'
 BIN_HEAD='/usr/bin/head'
@@ -20,6 +22,7 @@ BIN_AWK='/usr/bin/awk'
 BIN_BC='/usr/bin/bc'
 BIN_PS='/bin/ps'
 BIN_CUT='/usr/bin/cut'
+BIN_APT='/usr/bin/aptitude'
 BIN_XARGS='/usr/bin/xargs'
 
 COLOR_LIGHT_GREEN='\033[1;32m'
@@ -29,22 +32,19 @@ COLOR_RED='\033[0;31m'
 COLOR_WHITE='\033[1;37m'
 COLOR_DEFAULT='\033[0m'
 
-HEADER_FONT='smslant'
-
 echo -e "
-${COLOR_LIGHT_GREEN}$(${BIN_FIGLET} -ckw 80 -f ${HEADER_FONT} $(${BIN_HOSTNAME} -s))
-${COLOR_WHITE}Date and time             = ${COLOR_LIGHT_BLUE}$(${BIN_DATE})
-${COLOR_WHITE}Hostname                  = ${COLOR_LIGHT_BLUE}$(${BIN_HOSTNAME})
-${COLOR_WHITE}Global IP Addresses       = ${COLOR_LIGHT_BLUE}$(${BIN_IP} addr show eth0 2>/dev/null | ${BIN_GREP} 'inet ' | ${BIN_SED} -e 's/.*inet \([^ ]*\)\/.*/\1/' | ${BIN_SED} ':a;N;$!ba;s/\n/, /g')
-${COLOR_WHITE}VPN IP Address            = ${COLOR_LIGHT_BLUE}$(${BIN_IP} addr show tun0 2>/dev/null | ${BIN_GREP} 'inet ' | awk -F" " '{print $2}' | ${BIN_SED} ':a;N;$!ba;s/\n/, /g')
-${COLOR_WHITE}Uptime                    = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_CUT} -d "," -f 1 | xargs)
-${COLOR_WHITE}Load avg.                 = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_GREP} -o -e "load averages\?.*" | ${BIN_SED} -e "s/load averages\?: //")
-${COLOR_WHITE}Release                   = ${COLOR_LIGHT_BLUE}$(${BIN_AWK} -F= '/PRETTY_NAME=/ {gsub(/"/,""); print $2}' /etc/os-release)
-${COLOR_WHITE}Kernel                    = ${COLOR_LIGHT_BLUE}$(${BIN_UNAME} -ro)
-${COLOR_WHITE}CPU Usage (Core)          = ${COLOR_LIGHT_BLUE}$(echo $(${BIN_PS} -eo pcpu | ${BIN_AWK} 'NR>1' | ${BIN_AWK} '{tot=tot+$1} END {print tot}') / $(${BIN_CAT} /proc/cpuinfo | ${BIN_GREP} -c processor) | ${BIN_BC} )%
-${COLOR_WHITE}Memory         Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
-${COLOR_WHITE}Swap           Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
-${COLOR_WHITE}Disk(/)        Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_DF} -h / | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_DF} -h / | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})
+${COLOR_LIGHT_GREEN}$(${BIN_FIGLET} -ckw 80 -f slant $(${BIN_HOSTNAME} -s))
+${COLOR_WHITE}Date et heure                  = ${COLOR_LIGHT_BLUE}$(${BIN_DATE})
+${COLOR_WHITE}Hostname                       = ${COLOR_LIGHT_BLUE}$(${BIN_HOSTNAME})
+${COLOR_WHITE}Adresse IP                     = ${COLOR_LIGHT_BLUE}$(${BIN_IP} addr show eth0 2>/dev/null | ${BIN_GREP} 'inet ' | ${BIN_SED} -e 's/.*inet \([^ ]*\)\/.*/\1/' | ${BIN_SED} ':a;N;$!ba;s/\n/, /g')
+${COLOR_WHITE}Uptime                         = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_CUT} -d "," -f 1 | xargs)
+${COLOR_WHITE}Load avg.                      = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_GREP} -o -e "load averages\?.*" | ${BIN_SED} -e "s/load averages\?: //")
+${COLOR_WHITE}Release                        = ${COLOR_LIGHT_BLUE}$(${BIN_LSB_RELEASE} -d --short)
+${COLOR_WHITE}Mises à jour                   = ${COLOR_LIGHT_BLUE}$(${BIN_APT} search '~U' | wc -l )${COLOR_LIGHT_BLUE}" Mises à jour disponible"
+${COLOR_WHITE}Usage CPU                      = ${COLOR_LIGHT_BLUE}$(echo $(${BIN_PS} -eo pcpu | ${BIN_AWK} 'NR>1' | ${BIN_AWK} '{tot=tot+$1} END {print tot}') / $(${BIN_CAT} /proc/cpuinfo | ${BIN_GREP} -c processor) | ${BIN_BC} )%
+${COLOR_WHITE}Mémoire        Utilisée/Totale = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
+${COLOR_WHITE}Swap           Utilisée/Totale = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
+${COLOR_WHITE}Disque(/)      Utilisée/Totale = ${COLOR_LIGHT_BLUE}$(${BIN_DF} -h / | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_DF} -h / | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})
 ${COLOR_DEFAULT}"
 
 unset BIN_DATE BIN_FIGLET BIN_HOSTNAME BIN_IP BIN_GREP BIN_SED BIN_UPTIME BIN_CAT BIN_LSB_RELEASE BIN_UNAME BIN_FREE BIN_HEAD BIN_TAIL BIN_DF BIN_AWK BIN_BC BIN_PS
